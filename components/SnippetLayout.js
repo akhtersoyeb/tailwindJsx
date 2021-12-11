2e907c4d3d52e6b2c6a27da027cd74902f1f0168
Sk Soyeb Akhter <saleramesty2@gmail.com>
2021-12-11 12:17:33 +0530

    changed the name from masterblock to tailwindjsx

--- components/SnippetLayout.js
+++ components/SnippetLayout.js
@@ -21,7 +21,7 @@ export default function SnippetLayout({ children }) {
                         <Link href="/">
                             <a className="text-lg font-semibold flex items-center gap-3 text-white"><span><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 opacity-60" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16l-4-4m0 0l4-4m-4 4h18" />
-                            </svg></span> MasterCode</a>
+                            </svg></span> TailwindJSX</a>
                         </Link>
                     </div>
                 </div>
