d231ae14ed3bec794d77422b308de416baed9297
Sk Soyeb Akhter <saleramesty2@gmail.com>
2021-12-11 16:04:29 +0530

    upgraded tailwindcss to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    extend: {},
     fontFamily: {
       'sans': ['Poppins', 'sans-serif']
     },
-    extend: {},
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
