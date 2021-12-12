b8fd2a04b3a7edabb34a6e6690a25afe129ba5eb
Sk Soyeb Akhter <saleramesty2@gmail.com>
2021-12-12 12:06:33 +0530

    added favicon and title in the Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      
+      <Head>
+        <link rel="icon" href="/favicon_io/favicon.ico" />
+        <title>Tailwind JSX</title>
+      </Head>      
       <Component {...pageProps} />
     </>
   )
