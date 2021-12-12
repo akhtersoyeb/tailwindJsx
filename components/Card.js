69228772ae30686b3afc7434b0f74e5c58b291be
Sk Soyeb Akhter <saleramesty2@gmail.com>
2021-12-12 16:42:57 +0530

    fixed the bug that caused the card images to sqeeze

--- components/Card.js
+++ components/Card.js
@@ -2,9 +2,9 @@ import Image from "next/image"
 import Link from "next/link"
 export default function Card({ id, imageUrl }) {
     return (
-        <div className="bg-blue-400 rounded-lg hover:shadow-xl transform duration-75 h-60 e  w-full overflow-hidden">
+        <div className="bg-white rounded-lg border hover:shadow-xl transform duration-75 w-full h-60 overflow-hidden">
             <Link href={`snippets/${id}`}>
-                <img className="w-full h-full" src={imageUrl} alt="unable to load" />
+                <img className="w-full h-full object-scale-down" src={imageUrl} alt="unable to load" />
             </Link>
         </div>
     )
