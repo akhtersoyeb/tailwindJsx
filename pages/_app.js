cb6dd29d4c418f1b2d698a1ae2312e42cdce822e
Sk Soyeb Akhter <sksoyebakhter2003@gmail.com>
2024-08-31 23:45:28 +0530

    renamed project

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
-import '../styles/globals.css'
-import Head from 'next/head'
-
+import "../styles/globals.css"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon_io/favicon.ico" />
-        <title>Tailwind JSX</title>
-      </Head>      
+        <title>UI Snippets</title>
+      </Head>
       <Component {...pageProps} />
     </>
   )
